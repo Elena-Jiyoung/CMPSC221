7ab1982d7cef9a91aae7b7f6740baecc5021b282
Kinner Parikh <kinner@Kinners-MacBook-Pro.local>
2022-02-20 19:28:35 -0500

    did work

---
+++ CustomerLoanAccounts/src/customerloanaccounts/CarLoan.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package customerloanaccounts;
+
+/**
+ *
+ * @author kinner
+ */
+public class CarLoan extends LoanAccount {
+    private String vehicleVIN;
+
+    public CarLoan(double principle, double annualInterestRate, int months, String vehicleVIN) {
+        super(principle, annualInterestRate, months);
+        this.vehicleVIN = vehicleVIN;
+    }
+
+    @Override
+    public String toString() {
+        return "Car Loan with:" + super.toString() + "\nVehicle VIN: " + vehicleVIN + "\n\n";
+    }
+}
--- CustomerLoanAccounts/src/customerloanaccounts/Customer.java
+++ CustomerLoanAccounts/src/customerloanaccounts/Customer.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package loanaccounthierarchy;
+package customerloanaccounts;
 
 import java.util.ArrayList;
 
